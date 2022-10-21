cce6fade380f06107e88e4f399be13fcc0ea4b61
RdotSilva <RyandotSilva@gmail.com>
2022-10-21 21:03:03 +0100

    Add state for gold

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { Box, Container } from "@mui/material";
 
 type Props = {};
 
 const Main = (props: Props) => {
+  const [goldStats, setGoldStats] = useState({
+    gold: 0,
+    goldPerClick: 1,
+  });
+
+  const incrementGold = () => {
+    setGoldStats({ gold: goldStats.gold + 1, goldPerClick: 1 });
+  };
+
   return (
     <Container>
       <Box component="span" sx={{ display: "block" }}>
-        Gold
+        {goldStats.gold}
       </Box>
 
-      <Button variant="contained">Mine Gold</Button>
+      <Button onClick={incrementGold} variant="contained">
+        Mine Gold
+      </Button>
     </Container>
   );
 };
