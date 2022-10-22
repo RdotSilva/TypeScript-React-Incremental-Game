c8127691ee92e885bd8eda0c03bd10175423e5b2
RdotSilva <RyandotSilva@gmail.com>
2022-10-22 16:30:37 +0100

    Refactor Stats components

--- src/components/Stats/Ore.tsx
+++ src/components/Stats/Ore.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
-import useStat from "../../hooks/useStat";
-import Button from "@mui/material/Button";
-import { Box, Container } from "@mui/material";
+import React from "react";
+
+import Resource from "../Resource/Resource";
+import ore from "../../images/Icon46.png";
 
 type Props = {};
 
 const Ore = (props: Props) => {
-  const [stat, statPerClick, incrementStat, incrementStatPerClick] = useStat();
-
-  return (
-    <Container>
-      <Box component="span" sx={{ display: "block" }}>
-        <div>{stat}</div>
-      </Box>
-
-      <Button onClick={() => incrementStat(1)} variant="contained">
-        Mine Ore
-      </Button>
-      
-    </Container>
-  );
+  return <Resource image={ore} />;
 };
 
 export default Ore;
--- src/components/Stats/Tree.tsx
+++ src/components/Stats/Tree.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
-import useStat from "../../hooks/useStat";
-import Button from "@mui/material/Button";
-import { Box, Container } from "@mui/material";
+import React from "react";
+
+import Resource from "../Resource/Resource";
+import tree from "../../images/tree2.svg";
 
 type Props = {};
 
 const Tree = (props: Props) => {
-  const [stat, statPerClick, incrementStat, incrementStatPerClick] = useStat();
-
-  return (
-    <Container>
-      <Box component="span" sx={{ display: "block" }}>
-        <div>{stat}</div>
-      </Box>
-
-      <Button onClick={() => incrementStat(1)} variant="contained">
-        Chop Tree
-      </Button>
-    </Container>
-  );
+  return <Resource image={tree} />;
 };
 
 export default Tree;
