654d65edcd77e13e0190b1e54806aa566211910c
RdotSilva <RyandotSilva@gmail.com>
2022-10-22 15:05:12 +0100

    Clean up main app

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,28 +1,15 @@
-import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import { Box, Container } from "@mui/material";
+import React from "react";
+import { Container } from "@mui/material";
+import Gold from "../Stats/Ore";
+import Tree from "../Stats/Tree";
 
 type Props = {};
 
 const Main = (props: Props) => {
-  const [goldStats, setGoldStats] = useState({
-    gold: 0,
-    goldPerClick: 1,
-  });
-
-  const incrementGold = () => {
-    setGoldStats({ gold: goldStats.gold + 1, goldPerClick: 1 });
-  };
-
   return (
     <Container>
-      <Box component="span" sx={{ display: "block" }}>
-        {goldStats.gold}
-      </Box>
-
-      <Button onClick={incrementGold} variant="contained">
-        Mine Gold
-      </Button>
+      <Gold />
+      <Tree />
     </Container>
   );
 };
