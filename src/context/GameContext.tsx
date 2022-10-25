c2eb9831846b394fd3a6ce5eb88592427cee1d7a
RdotSilva <RyandotSilva@gmail.com>
2022-10-25 19:12:12 +0100

    Update context to include tier

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -1,12 +1,13 @@
+import * as React from "react";
 interface Stat {
   stat: number;
 }
 export type StatContextType = {
   stat: number;
   setStat: React.Dispatch<React.SetStateAction<number>>;
+  tier: number;
+  setTier: React.Dispatch<React.SetStateAction<number>>;
 };
-
-import * as React from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -16,9 +17,10 @@ export const StatContext = React.createContext<StatContextType | null>(null);
 
 const StatProvider = ({ children }: Props) => {
   const [stat, setStat] = React.useState(1);
+  const [tier, setTier] = React.useState(0);
 
   return (
-    <StatContext.Provider value={{ stat, setStat }}>
+    <StatContext.Provider value={{ stat, setStat, tier, setTier }}>
       {children}
     </StatContext.Provider>
   );
