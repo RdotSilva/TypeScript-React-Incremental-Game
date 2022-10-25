f09a156d5efd1559d46f673499e78f0fcbd5b5b5
RdotSilva <RyandotSilva@gmail.com>
2022-10-25 19:11:18 +0100

    Add OreNew component for testing

---
+++ src/components/Stats/OreNew.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useContext, useEffect } from "react";
+import useStat from "../../hooks/useStat";
+import { Box, Container } from "@mui/material";
+import Upgrade from "../Upgrade/Upgrade";
+
+import Resource from "../Resource/Resource";
+import ore from "../../images/Icon46.png";
+import { StatContext } from "../../context/GameContext";
+
+interface Props {}
+
+const OreNew = (props: Props) => {
+  const statContext = useContext(StatContext);
+
+  const [
+    stat,
+    statPerClick,
+    incrementStat,
+    incrementStatPerClick,
+    autoIncrementStat,
+  ] = useStat();
+  const [totalUpgrades, setTotalUpgrades] = useState<number>(0);
+
+  /**
+   * Upgrade a resource and increase the stat per click for that particular resource
+   * @param amount The amount to increase the stat per click
+   */
+  const upgrade = (amount: number) => {
+    incrementStatPerClick(amount);
+    setTotalUpgrades(totalUpgrades + 1);
+
+    // Add the auto stat click after the 3rd upgrade
+    if (totalUpgrades >= 2) {
+      autoIncrementStat();
+    }
+  };
+
+  useEffect(() => {
+    if (stat > 100) {
+      statContext?.setTier(2);
+    }
+  }, [stat]);
+
+  return (
+    <Container>
+      <Box component="span" sx={{ display: "block" }}>
+        <div>{stat}</div>
+      </Box>
+
+      <Box>
+        <img src={ore} alt="tree" onClick={() => incrementStat(1)} />
+      </Box>
+
+      <Upgrade
+        statTotal={stat}
+        totalUpgrades={totalUpgrades}
+        upgrade={upgrade}
+        tier={1}
+      />
+    </Container>
+  );
+};
+
+export default OreNew;
