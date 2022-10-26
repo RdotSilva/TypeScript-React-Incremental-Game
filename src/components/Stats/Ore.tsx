bdb555dd1491a2abd5233b9b524a08eb3412e211
RdotSilva <RyandotSilva@gmail.com>
2022-10-26 20:52:17 +0100

    Refactor using new config enum

--- src/components/Stats/Ore.tsx
+++ src/components/Stats/Ore.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import Resource from "../Resource/Resource";
 import ore from "../../images/Icon46.png";
+import { TierLevels } from "../../config/tierLevels";
 
 type Props = {};
 
 const Ore = (props: Props) => {
-  return <Resource image={ore} tier={1} nextTierThreshold={100} />;
+  return <Resource image={ore} tier={1} nextTierThreshold={TierLevels.One} />;
 };
 
 export default Ore;
--- src/components/Stats/Tree.tsx
+++ src/components/Stats/Tree.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import Resource from "../Resource/Resource";
 import tree from "../../images/tree2.svg";
+import { TierLevels } from "../../config/tierLevels";
 
 type Props = {};
 
 const Tree = (props: Props) => {
-  return <Resource image={tree} tier={2} nextTierThreshold={120} />;
+  return <Resource image={tree} tier={2} nextTierThreshold={TierLevels.Two} />;
 };
 
 export default Tree;
