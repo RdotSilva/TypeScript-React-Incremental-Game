271db53fcf028462a86c6bb412b739a2003f15c1
RdotSilva <RyandotSilva@gmail.com>
2022-10-28 21:03:21 +0100

    Add display for new resource

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,15 +3,18 @@ import { Container } from "@mui/material";
 import Ore from "../Stats/Ore";
 import Tree from "../Stats/Tree";
 import { StatContext } from "../../context/GameContext";
+import Skull from "../Stats/Skull";
 
 type Props = {};
 
 const Main = (props: Props) => {
   const statContext = useContext(StatContext);
+
   return (
     <Container>
       <Ore />
       {statContext?.tier! >= 2 && <Tree />}
+      {statContext?.tier! >= 3 && <Skull />}
     </Container>
   );
 };
