d41db01358ce70a6804ae6360ce17291ebe52ed9
RdotSilva <RyandotSilva@gmail.com>
2022-10-31 11:46:31 -0400

    Update context to include total stats

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -7,6 +7,9 @@ export type StatContextType = {
   setStat: React.Dispatch<React.SetStateAction<number>>;
   tier: number;
   setTier: React.Dispatch<React.SetStateAction<number>>;
+  totalStats: number;
+  setTotalStats: React.Dispatch<React.SetStateAction<number>>;
+
 };
 
 type Props = {
@@ -18,9 +21,10 @@ export const StatContext = React.createContext<StatContextType | null>(null);
 const StatProvider = ({ children }: Props) => {
   const [stat, setStat] = React.useState(1);
   const [tier, setTier] = React.useState(0);
+  const [totalStats, setTotalStats] = React.useState(0);
 
   return (
-    <StatContext.Provider value={{ stat, setStat, tier, setTier }}>
+    <StatContext.Provider value={{ stat, setStat, tier, setTier, totalStats, setTotalStats }}>
       {children}
     </StatContext.Provider>
   );
