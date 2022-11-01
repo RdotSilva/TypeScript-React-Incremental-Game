e6a56f368e18bdf422f0f62fc5f46fd20448022b
RdotSilva <RyandotSilva@gmail.com>
2022-11-01 08:49:23 -0400

    Add Teeth component

---
+++ src/components/Stats/Teeth.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Resource from "../Resource/Resource";
+import teeth from "../../images/resources/teeth.png";
+import { TierLevels } from "../../config/config";
+
+type Props = {};
+
+const Teeth = (props: Props) => {
+  return (
+    <Resource
+      image={tree}
+      resourceTier={4}
+      nextTierThreshold={TierLevels.Two}
+    />
+  );
+};
+
+export default Teeth;
