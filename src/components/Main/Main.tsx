178dca7ebf9be6bd4ed11bf41923afb0b167f66f
RdotSilva <RyandotSilva@gmail.com>
2022-11-02 20:40:32 +0000

    Update main app layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,6 +5,7 @@ import Tree from "../Stats/Tree";
 import { StatContext } from "../../context/GameContext";
 import Skull from "../Stats/Skull";
 import Brain from "../Stats/Brain";
+import PowerUp from "../PowerUp/PowerUp";
 
 type Props = {};
 
@@ -12,12 +13,15 @@ const Main = (props: Props) => {
   const statContext = useContext(StatContext);
 
   return (
-    <Container sx={{ background: "gray" }}>
-      <Ore />
-      {statContext?.tier! >= 2 && <Tree />}
-      {statContext?.tier! >= 3 && <Skull />}
-      {statContext?.tier! >= 4 && <Brain />}
-    </Container>
+    <>
+      <Container sx={{ background: "gray" }}>
+        <Ore />
+        {statContext?.tier! >= 2 && <Tree />}
+        {statContext?.tier! >= 3 && <Skull />}
+        {statContext?.tier! >= 4 && <Brain />}
+      </Container>
+      <PowerUp />
+    </>
   );
 };
 
