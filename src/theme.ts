bab6b5f115cd528550d6bc6a91f448fdd7bbfa4f
RdotSilva <RyandotSilva@gmail.com>
2022-11-13 19:01:29 +0000

    Update theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,16 @@
 import { createTheme } from "@mui/material/styles";
 import { orange, red } from "@mui/material/colors";
 
+const lightTan = "#ede7d5";
+const darkTan = "#d4cebe";
+
 const theme = createTheme({
-  status: {
-    danger: red[500],
-  },
   palette: {
     primary: {
-      main: orange[500],
+      main: lightTan,
+    },
+    secondary: {
+      main: darkTan,
     },
   },
 });
