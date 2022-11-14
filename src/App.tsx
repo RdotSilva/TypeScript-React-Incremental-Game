8c891810f20d492a131e819e029ce4752212ff9b
RdotSilva <RyandotSilva@gmail.com>
2022-11-14 20:35:19 +0000

    Remove custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import Main from "./components/Main/Main";
 import StatProvider from "./context/StatContext";
 
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StatProvider>
-        <div className="App">
-          <Main />
-        </div>
-      </StatProvider>
-    </ThemeProvider>
+    <StatProvider>
+      <div className="App">
+        <Main />
+      </div>
+    </StatProvider>
   );
 }
 
