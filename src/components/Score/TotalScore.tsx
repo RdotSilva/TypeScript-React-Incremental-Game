c76a9a1065bf7635a75df56431edc57f86ca1f6d
RdotSilva <RyandotSilva@gmail.com>
2022-11-16 17:44:05 +0000

    Add StyledScoreContainer

--- src/components/Score/TotalScore.tsx
+++ src/components/Score/TotalScore.tsx
@@ -2,25 +2,36 @@ import React, { useContext, useEffect, useState, useCallback } from "react";
 import { Box, Grid } from "@mui/material";
 import { StatContext } from "../../context/StatContext";
 import { COLORS } from "../../config/colors";
+import { styled } from "@mui/system";
 
 type Props = {};
+
+const StyledScoreContainer = styled(Grid)({
+  backgroundColor: COLORS.lightTan,
+  display: "flex",
+  justifyContent: "center",
+});
 
 const TotalScore = (props: Props) => {
   const statContext = useContext(StatContext);
   const { totalStats } = statContext;
 
   return (
-    <Grid
-      sx={{
-        backgroundColor: COLORS.darkTan,
-        display: "flex",
-        justifyContent: "center",
-        fontSize: "h2.fontSize",
-        fontFamily: "Monospace",
-      }}
-    >
-      {totalStats}
-    </Grid>
+    <StyledScoreContainer container>
+      <Grid
+        xs={2}
+        item
+        sx={{
+          backgroundColor: COLORS.darkTan,
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "h2.fontSize",
+          fontFamily: "Monospace",
+        }}
+      >
+        {totalStats}
+      </Grid>
+    </StyledScoreContainer>
   );
 };
 
