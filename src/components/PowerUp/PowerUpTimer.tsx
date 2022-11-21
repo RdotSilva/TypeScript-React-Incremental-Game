f09914acd62f469bab9c5e4d64ee72cf60f86ee6
RdotSilva <RyandotSilva@gmail.com>
2022-11-21 18:27:30 +0000

    Fix power up timer display

--- src/components/PowerUp/PowerUpTimer.tsx
+++ src/components/PowerUp/PowerUpTimer.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StatContext } from "../../context/StatContext";
 
 type Props = {};
 
 const PowerUpTimer = (props: Props) => {
-  return <div>PowerUp Timer: 1000</div>;
+  const statContext = useContext(StatContext);
+  const { powerUpTimer } = statContext;
+
+  return <div>{powerUpTimer / 1000}</div>;
 };
 
 export default PowerUpTimer;
