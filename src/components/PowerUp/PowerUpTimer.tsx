6d17fde83dc466b82da28bfa4243a90c678fbbfb
RdotSilva <RyandotSilva@gmail.com>
2022-11-23 20:38:00 +0000

    Update display for timer

--- src/components/PowerUp/PowerUpTimer.tsx
+++ src/components/PowerUp/PowerUpTimer.tsx
@@ -1,5 +1,16 @@
+import { Grid } from "@mui/material";
+import { styled } from "@mui/system";
 import React, { useContext } from "react";
+import { COLORS } from "../../config/colors";
 import { StatContext } from "../../context/StatContext";
+
+const StyledPowerUpTimerDisplayContainer = styled(Grid)({
+  backgroundColor: COLORS.lightPurple,
+  display: "flex",
+  justifyContent: "center",
+  fontSize: "25px",
+  width: "100%",
+});
 
 type Props = {};
 
@@ -7,7 +18,11 @@ const PowerUpTimer = (props: Props) => {
   const statContext = useContext(StatContext);
   const { powerUpTimer } = statContext;
 
-  return <div>{powerUpTimer / 1000}</div>;
+  return (
+    <StyledPowerUpTimerDisplayContainer item>
+      {`2x XP: ${powerUpTimer / 1000}`}
+    </StyledPowerUpTimerDisplayContainer>
+  );
 };
 
 export default PowerUpTimer;
