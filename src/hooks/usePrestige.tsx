54888fe04ef031ed10a258c09df68ffff7512b57
RdotSilva <RyandotSilva@gmail.com>
2022-12-01 18:23:23 +0000

    Add TODO for prestige logic

--- src/hooks/usePrestige.tsx
+++ src/hooks/usePrestige.tsx
@@ -1,16 +1,40 @@
 import React, { useContext } from "react";
 import { StatContext } from "../context/StatContext";
+import useStat from "./useStat";
 
 /**
  * Main hook used for handling prestige logic
  */
 const usePrestige = (): any => {
   const statContext = useContext(StatContext);
+  const { resetStat, totalStats, setTotalStats, stat } = useStat();
+
   const { prestigeLevel, setPrestigeLevel } = statContext;
+
+  const prestigeScore = 100;
+
+  // TODO: Implement prestige logic
+  const confirmPrestige = () => {
+    console.log("confirming prestige");
+    resetStat();
+    setTotalStats(0);
+    setPrestigeLevel(1);
+
+    console.log(stat);
+    // If total score is greater than or equal to the SCORE_TO_PRESTIGE
+
+    // if (totalStats >= prestigeScore) {
+
+    // }
+    // Set prestige level ++
+    // Set score back to zero
+    // Set all resources back to ZERO
+  };
 
   return {
     prestigeLevel,
     setPrestigeLevel,
+    confirmPrestige,
   };
 };
 
