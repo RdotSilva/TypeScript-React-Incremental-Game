236f04beb41f96bdf74fcefb4a064f9c960f9137
RdotSilva <RyandotSilva@gmail.com>
2022-12-02 20:01:06 +0000

    Add new context for prestige

---
+++ src/context/PrestigeContext.tsx
@@ -0,0 +1,43 @@
+import React, {
+  useState,
+  createContext,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+export type PrestigeContextType = {
+  prestigeLevel: number;
+  setPrestigeLevel: Dispatch<SetStateAction<number>>;
+  isActivatingPrestige: boolean;
+  setIsActivatingPrestige: Dispatch<SetStateAction<boolean>>;
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export const PrestigeContext = createContext<PrestigeContextType>(
+  {} as PrestigeContextType
+);
+
+const StatProvider = ({ children }: Props) => {
+  const [prestigeLevel, setPrestigeLevel] = useState<number>(0);
+  const [isActivatingPrestige, setIsActivatingPrestige] =
+    useState<boolean>(false);
+
+  return (
+    <PrestigeContext.Provider
+      value={{
+        prestigeLevel,
+        setPrestigeLevel,
+        isActivatingPrestige,
+        setIsActivatingPrestige,
+      }}
+    >
+      {children}
+    </PrestigeContext.Provider>
+  );
+};
+
+export default StatProvider;
