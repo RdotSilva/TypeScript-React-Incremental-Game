2c0b012a74e15c98d148f4d5cf1f7fba18201ce8
RdotSilva <RyandotSilva@gmail.com>
2022-12-03 14:43:32 +0000

    Add config for prestige

---
+++ src/config/prestige.ts
@@ -0,0 +1,11 @@
+/**
+ * The number of prestige points that will be given for a particular level threshold
+ * If the user passes threshold of 100 (One) they will receive 1 prestige point
+ */
+export const enum PrestigePointsPreLevel {
+  One = 1,
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Five = 5,
+}
