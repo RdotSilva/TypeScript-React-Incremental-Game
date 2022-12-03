9e10ec47884b40428ee815e55b94c73350501565
RdotSilva <RyandotSilva@gmail.com>
2022-12-03 14:32:45 +0000

    Add confirmation dialog for prestige

--- src/components/Prestige/Prestige.tsx
+++ src/components/Prestige/Prestige.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { Button, Grid, styled } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  styled,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import { COLORS } from "../../config/colors";
 import usePrestige from "../../hooks/usePrestige";
 
@@ -12,18 +21,55 @@ const StyledPrestigeContainer = styled(Grid)({
 type Props = {};
 
 const Prestige = (props: Props) => {
+  const [openPrestigeConfirmationModal, setOpenPrestigeConfirmationModal] =
+    useState(false);
+
   const { confirmPrestige } = usePrestige();
+
+  const onClickOpen = () => {
+    setOpenPrestigeConfirmationModal(true);
+  };
+
+  const onClickClose = () => {
+    setOpenPrestigeConfirmationModal(false);
+  };
 
   return (
     <StyledPrestigeContainer>
       <Button
         color="secondary"
         sx={{ m: 1 }}
-        onClick={() => confirmPrestige()}
+        onClick={onClickOpen}
         variant="contained"
       >
         Prestige
       </Button>
+      <Dialog
+        PaperProps={{
+          style: { backgroundColor: COLORS.lightTan },
+        }}
+        open={openPrestigeConfirmationModal}
+        onClose={onClickClose}
+        aria-labelledby="confirm-prestige-dialog-title"
+        aria-describedby="confirm-prestige-dialog-description"
+      >
+        <DialogTitle id="confirm-prestige-dialog-title">
+          Are you sure you want to prestige?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="confirm-prestige-dialog-description">
+            WARNING: Confirm prestige will reset your total stats and all
+            individual resource stats back to 0. You will gain prestige points
+            that can be used to make your next play through quicker.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClickClose}>Go Back</Button>
+          <Button onClick={confirmPrestige} autoFocus>
+            Confirm Prestige
+          </Button>
+        </DialogActions>
+      </Dialog>
     </StyledPrestigeContainer>
   );
 };
