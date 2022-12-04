4df56bef2c2a64707c101467c16eff44cc49fbc1
RdotSilva <RyandotSilva@gmail.com>
2022-12-04 20:46:08 +0000

    Add PrestigeMenu component

---
+++ src/components/Prestige/PrestigeMenu/PrestigeMenu.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import AccessAlarmRoundedIcon from "@mui/icons-material/AccessAlarmRounded";
+import Battery0BarRoundedIcon from "@mui/icons-material/Battery0BarRounded";
+import BeachAccessRoundedIcon from "@mui/icons-material/BeachAccessRounded";
+
+type Props = {};
+
+const PrestigeMenu = (props: Props) => {
+  return (
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <AccessAlarmRoundedIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Time Warp" secondary="Increase speed 2x" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <Battery0BarRoundedIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Super Charge" secondary="Increase score by 2x" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessRoundedIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary="Make it Rain"
+          secondary="Increase change for power ups to appear"
+        />
+      </ListItem>
+    </List>
+  );
+};
+
+export default PrestigeMenu;
