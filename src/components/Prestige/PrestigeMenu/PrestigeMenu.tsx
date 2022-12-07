d945857f27910fef8d7ee6f0ad5be555ad76edf2
RdotSilva <RyandotSilva@gmail.com>
2022-12-07 18:16:45 +0000

    Add logic for mapping through prestige menu items

--- src/components/Prestige/PrestigeMenu/PrestigeMenu.tsx
+++ src/components/Prestige/PrestigeMenu/PrestigeMenu.tsx
@@ -4,12 +4,12 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
-import AccessAlarmRoundedIcon from "@mui/icons-material/AccessAlarmRounded";
-import Battery0BarRoundedIcon from "@mui/icons-material/Battery0BarRounded";
-import BeachAccessRoundedIcon from "@mui/icons-material/BeachAccessRounded";
+
 import { Button } from "@mui/material";
 import { COLORS } from "../../../config/colors";
 import usePrestige from "../../../hooks/usePrestige";
+import PrestigeMenuItem from "../PrestigeMenuItem/PrestigeMenuItem";
+import { PrestigeMenuItems } from "../../../config/prestige";
 
 type Props = {};
 
@@ -46,36 +46,19 @@ const PrestigeMenu = (props: Props) => {
             Prestige Points {assignedPrestigePoints} / {totalPrestigePoints}{" "}
             total points assigned.
           </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <AccessAlarmRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Time Warp" secondary="Increase speed 2x" />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <Battery0BarRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="Super Charge"
-              secondary="Increase score by 2x"
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <BeachAccessRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="Make it Rain"
-              secondary="Increase change for power ups to appear"
-            />
-          </ListItem>
+          {PrestigeMenuItems.map(
+            ({
+              prestigeItemIcon,
+              prestigeItemTitle,
+              prestigeItemDescription,
+            }) => {
+              <PrestigeMenuItem
+                prestigeItemIcon={prestigeItemIcon}
+                prestigeItemTitle={prestigeItemTitle}
+                prestigeItemDescription={prestigeItemDescription}
+              />;
+            }
+          )}
         </List>
       ) : null}
     </>
