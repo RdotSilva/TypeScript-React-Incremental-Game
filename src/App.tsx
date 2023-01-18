603b2eebe0cf027a8c1232c776de96860e49125d
RdotSilva <RyandotSilva@gmail.com>
2023-01-18 19:49:31 +0000

    Move context to main App file

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import StatProvider from "./context/StatContext";
 
 import { ThemeProvider } from "@mui/material/styles";
 import PrestigeProvider from "./context/PrestigeContext";
+import store, { StoreContext } from "./store";
 
 function App() {
   return (
-    <StatProvider>
+    <StoreContext.Provider value={store}>
       <PrestigeProvider>
         <div className="App">
           <Main />
         </div>
       </PrestigeProvider>
-    </StatProvider>
+    </StoreContext.Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store, { StoreContext } from "./store";
+import { StoreContext } from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <StoreContext.Provider value={store}>
-      <App />
-    </StoreContext.Provider>
+    <App />
   </React.StrictMode>
 );
 
