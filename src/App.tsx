dcfe37b737e11124c06acc5ddb1d4aa23a3ea4e3
RdotSilva <RyandotSilva@gmail.com>
2023-02-02 21:47:54 +0000

    Remove unused context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import Main from "./components/Main/Main";
-import StatProvider from "./context/StatContext";
 
-import { ThemeProvider } from "@mui/material/styles";
-import PrestigeProvider from "./context/PrestigeContext";
 import store, { StoreContext } from "./store";
 
 function App() {
   return (
-    <StatProvider>
-      <StoreContext.Provider value={store}>
-        <PrestigeProvider>
-          <div className="App">
-            <Main />
-          </div>
-        </PrestigeProvider>
-      </StoreContext.Provider>
-    </StatProvider>
+    <StoreContext.Provider value={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </StoreContext.Provider>
   );
 }
 
