02074935bf26f36259895b7a1f9d56aef8fcc590
RdotSilva <RyandotSilva@gmail.com>
2023-02-06 21:24:26 +0000

    Add powerUpStore to main store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { createContext, useContext } from "react";
+import PowerUpStore from "./stores/PowerUpStore";
 import PrestigeStore from "./stores/PrestigeStore";
 import StatStore from "./stores/StatStore";
 
 const store = {
   statStore: new StatStore(),
   prestigeStore: new PrestigeStore(),
+  powerUpStore: new PowerUpStore(),
 };
 
 export const StoreContext = createContext(store);
