b7d257fc512e0bdf6d471f78c333ed9918b0bbff
RdotSilva <RyandotSilva@gmail.com>
2023-02-10 19:39:19 +0000

    Replace TODOs with assertions

--- tests/Main/main.spec.ts
+++ tests/Main/main.spec.ts
@@ -2,15 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-
-  // Expect a title "to contain" a substring
   await expect(page).toHaveTitle(/Incremental Game/);
 
-  // TODO: Check default score set to 0
-
-  // TODO: Check current bonus default None
-
-   // TODO: Check only first resource is displayed
-  
-   // TODO: Check first resource score is defaulted to 0
+  // Check default values when new game is started
+  await expect(page.locator(".total-stats")).toHaveText("0");
+  await expect(page.locator(".current-bonus")).toHaveText(
+    "Current Bonus: None"
+  );
+  await expect(page.locator(".Icon1-stat")).toHaveText("0");
 });
