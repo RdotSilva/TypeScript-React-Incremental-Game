424fb3f6ec2793a78361714f154fedf6ef7e2426
RdotSilva <RyandotSilva@gmail.com>
2023-02-11 11:22:51 +0000

    Split tests and add describe

--- tests/Main/main.spec.ts
+++ tests/Main/main.spec.ts
@@ -1,13 +1,29 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page).toHaveTitle(/Incremental Game/);
+test.describe("Main application", () => {
+  test("has correct title", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page).toHaveTitle(/Incremental Game/);
 
-  // Check default values when new game is started
-  await expect(page.locator(".total-stats")).toHaveText("0");
-  await expect(page.locator(".current-bonus")).toHaveText(
-    "Current Bonus: None"
-  );
-  await expect(page.locator(".Icon1-stat")).toHaveText("0");
+    // Check default values when new game is started
+    await expect(page.locator(".total-stats")).toHaveText("0");
+    await expect(page.locator(".current-bonus")).toHaveText(
+      "Current Bonus: None"
+    );
+    await expect(page.locator(".Icon1-stat")).toHaveText("0");
+  });
+
+  test("has correct default values when starting a new game", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page).toHaveTitle(/Incremental Game/);
+
+    // Check default values when new game is started
+    await expect(page.locator(".total-stats")).toHaveText("0");
+    await expect(page.locator(".current-bonus")).toHaveText(
+      "Current Bonus: None"
+    );
+    await expect(page.locator(".Icon1-stat")).toHaveText("0");
+  });
 });
