49388781ac1943654f7bf0d8418e3cebc841481c
RdotSilva <RyandotSilva@gmail.com>
2023-02-12 20:33:56 +0000

    Add TODO tests

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -1,1 +1,7 @@
-// TODO: Add tests for resource
+import { test, expect } from "@playwright/test";
+
+test.fixme("check default state", async ({ page }) => {});
+
+test.fixme("check stat increase on click", async ({ page }) => {});
+
+test.fixme("check upgrade buttons", async ({ page }) => {});
