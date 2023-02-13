8bf0d2e06e6854beb1e763341927c612ea13a1af
RdotSilva <RyandotSilva@gmail.com>
2023-02-13 18:56:29 +0000

    Add test for default state

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test.fixme("check default state", async ({ page }) => {});
+const resourceOneIconSelector = ".Icon1-stat";
+
+test("check default state", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  page.locator(resourceOneIconSelector).click();
+  await expect(page.locator(resourceOneIconSelector)).toHaveText("0");
+});
 
 test.fixme("check stat increase on click", async ({ page }) => {});
 
