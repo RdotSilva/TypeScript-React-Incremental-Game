5eaf04fc6781e78a2ce93ed70d6bababd14617a1
RdotSilva <RyandotSilva@gmail.com>
2023-02-14 20:14:32 +0000

    Add test for stat increase with no upgrade

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -6,3 +6,15 @@ test("check default state", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await expect(page.locator(resourceOneIconSelector)).toHaveText("0");
 });
+
+test("check stat increase by 1 on click when no upgrades are purchased", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/");
+  await page.locator(resourceOneIconSelector).click();
+  await expect(page.locator(resourceOneIconSelector)).toHaveText("1");
+  await page.locator(resourceOneIconSelector).click();
+  await expect(page.locator(resourceOneIconSelector)).toHaveText("2");
+  await page.locator(resourceOneIconSelector).click();
+  await expect(page.locator(resourceOneIconSelector)).toHaveText("3");
+});
