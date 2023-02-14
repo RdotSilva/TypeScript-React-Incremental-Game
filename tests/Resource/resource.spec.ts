0bcbe8dc2310ae8ab5f1d96fc56f7907783b38c0
RdotSilva <RyandotSilva@gmail.com>
2023-02-14 20:13:24 +0000

    Fix test for default state

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -4,20 +4,5 @@ const resourceOneIconSelector = ".Icon1-stat";
 
 test("check default state", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  page.locator(resourceOneIconSelector).click();
   await expect(page.locator(resourceOneIconSelector)).toHaveText("0");
 });
-
-test("check stat increase by 1 on click when no upgrades are purchased", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:3000/");
-  page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("1");
-  page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("2");
-  page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("3");
-});
-
-test.fixme("check upgrade buttons", async ({ page }) => {});
