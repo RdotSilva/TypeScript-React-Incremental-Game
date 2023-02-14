c16213fff3722a5db2a598b0acbb175d5826cf04
RdotSilva <RyandotSilva@gmail.com>
2023-02-14 20:28:32 +0000

    Add test for first upgrade button

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 const resourceOneIconSelector = ".Icon1-stat";
+const doubleStatUpgradeButtonSelector = 'button:text("x2 per click")';
 
 test("check default state", async ({ page }) => {
   await page.goto("http://localhost:3000/");
@@ -17,4 +18,22 @@ test("check stat increase by 1 on click when no upgrades are purchased", async (
   await expect(page.locator(resourceOneIconSelector)).toHaveText("2");
   await page.locator(resourceOneIconSelector).click();
   await expect(page.locator(resourceOneIconSelector)).toHaveText("3");
+});
+
+test("check upgrade buttons", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+  await page.locator(resourceOneIconSelector).click();
+
+  await expect(
+    page.getByRole("button", { name: "X2 PER CLICK" })
+  ).toBeVisible();
 });
