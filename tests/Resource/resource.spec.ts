8be229451e2bfac8b03951db24c0b4213f2080f4
RdotSilva <RyandotSilva@gmail.com>
2023-02-17 22:52:27 +0000

    Refactor tests

--- tests/Resource/resource.spec.ts
+++ tests/Resource/resource.spec.ts
@@ -3,38 +3,40 @@ import { test, expect } from "@playwright/test";
 const resourceOneIconSelector = ".Icon1-stat";
 const doubleStatUpgradeButtonSelector = 'button:text("x2 per click")';
 
-test("check default state", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("0");
-});
-
-test("check stat increase by 1 on click when no upgrades are purchased", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:3000/");
-  await page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("1");
-  await page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("2");
-  await page.locator(resourceOneIconSelector).click();
-  await expect(page.locator(resourceOneIconSelector)).toHaveText("3");
-});
-
-test("check upgrade buttons", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await page.getByRole("img", { name: "Icon1" }).click({
-    clickCount: 10,
+test.describe("Resource", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+  });
+  test("check default state", async ({ page }) => {
+    await expect(page.locator(resourceOneIconSelector)).toHaveText("0");
   });
 
-  await expect(page.getByRole("button", { name: "X2 PER CLICK" })).toHaveCount(
-    1
-  );
-
-  await page.getByRole("img", { name: "Icon1" }).click({
-    clickCount: 90,
+  test("check stat increase by 1 on click when no upgrades are purchased", async ({
+    page,
+  }) => {
+    await page.locator(resourceOneIconSelector).click();
+    await expect(page.locator(resourceOneIconSelector)).toHaveText("1");
+    await page.locator(resourceOneIconSelector).click();
+    await expect(page.locator(resourceOneIconSelector)).toHaveText("2");
+    await page.locator(resourceOneIconSelector).click();
+    await expect(page.locator(resourceOneIconSelector)).toHaveText("3");
   });
 
-  await expect(page.getByRole("button", { name: "X2 PER CLICK" })).toHaveCount(
-    2
-  );
+  test("check upgrade buttons", async ({ page }) => {
+    await page.getByRole("img", { name: "Icon1" }).click({
+      clickCount: 10,
+    });
+
+    await expect(
+      page.getByRole("button", { name: "X2 PER CLICK" })
+    ).toHaveCount(1);
+
+    await page.getByRole("img", { name: "Icon1" }).click({
+      clickCount: 90,
+    });
+
+    await expect(
+      page.getByRole("button", { name: "X2 PER CLICK" })
+    ).toHaveCount(2);
+  });
 });
