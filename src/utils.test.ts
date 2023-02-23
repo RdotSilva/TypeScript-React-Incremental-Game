d35f93bc8bd624dee7f8002a24e613b3bacac40b
RdotSilva <RyandotSilva@gmail.com>
2023-02-23 20:39:55 +0000

    Add test for getRandomObjectProperty

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,11 +1,24 @@
 import { getRandomEnumValue, getRandomObjectProperty } from "./utils";
 
+global.Math.random = () => 0.5;
+
 describe("src/utils.ts", () => {
   describe("getRandomEnumValue", () => {
     it.todo("should return a random value from an Enum");
   });
 
   describe("getRandomObjectProperty", () => {
-    it.todo("should return a random property from an object");
+    it("should return a random property from an object", () => {
+      const object = {
+        One: 2,
+        Two: 4,
+        Three: 8,
+        Four: 16,
+      };
+
+      const randomProperty = getRandomObjectProperty(object);
+
+      console.log(randomProperty);
+    });
   });
 });
