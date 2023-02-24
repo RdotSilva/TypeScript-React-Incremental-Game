7bff0ac4581c00818cfc0c4211dc9d55edbd8884
RdotSilva <RyandotSilva@gmail.com>
2023-02-24 20:03:54 +0000

    Remove unused util function

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,3 @@
-/**
- * Get a random value from an enum
- * @param enumeration The enum to get the value from
- * @returns Random value
- */
-export const getRandomEnumValue = (enumeration: any): any => {
-  const values = Object.keys(enumeration);
-  const enumKey = values[Math.floor(Math.random() * values.length)];
-  return enumeration[enumKey];
-};
-
 export const getRandomObjectProperty = (obj: any) => {
   const keys = Object.keys(obj);
   return obj[keys[(keys.length * Math.random()) << 0]];
