e4bccdd46df4a39041dfd8160145cd206d2c4379
RdotSilva <RyandotSilva@gmail.com>
2023-02-25 19:18:21 +0000

    Add TODOs for test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Main component ", () => {
+test("renders Main component default state", () => {
   render(<App />);
+
   const currentBonusElement = screen.getByText(/Current Bonus: None/i);
+
   expect(currentBonusElement).toBeInTheDocument();
+
+  // TODO: Check initial total stat display number is 0
+
+  // TODO: Check resource initial stat display is 0
 });
